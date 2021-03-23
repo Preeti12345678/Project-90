977a84103e18021c63323af821c3d73b987c91a7
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-03-23 15:09:53 +0530

    Add files via upload

--- quiz_login.js
+++ quiz_login.js
@@ -1,7 +1,7 @@
 function addUser(){
     player1_name=document.getElementById("player1_name_input").value;
     player2_name=document.getElementById("player2_name_input").value;
-    localStorage.setItem("Player 1 name", player1_name);
-    localStorage.setItem("player 2 name", player2_name);
+    localStorage.setItem("Player1_name", player1_name);
+    localStorage.setItem("Player2_name", player2_name);
     window.location.replace("quiz.html");
 }
